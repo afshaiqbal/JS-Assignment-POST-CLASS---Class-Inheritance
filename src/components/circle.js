d2e131b7c648157a5e10287bfec570b2d2432ac2
sshubham681 <sshubham681@gmail.com>
2021-08-24 21:52:51 +0530

    Done

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,15 @@
 
 
 // export class using module.exports
+
+var Shape = require('./shape.js');
+class Circle extends Shape {
+  constructor() {
+    super();
+    this.radius = 9;
+  }
+  calculateArea() {
+    return "Calculated area of circle";
+  }
+}
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
 
 // export class using module.exports
+class Shape {
+  constructor() {
+    this.color = 'blue';
+  }
+  drawShape() {
+    return "A shape is drawn";
+  }
+  calculateArea() {
+    return "Area is calulated. ";
+  }
+}
+
+module.exports = Shape;
